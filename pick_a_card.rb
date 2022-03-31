eb9ceee99238d785b96a2852583b047f8c05faa9
ale.mishchenko <ale-ivankova@yandex.ru>
2022-03-31 11:51:27 +0300

    unicode version

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,12 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
 
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suits = %w[Diamonds Hearts Clubs Spades]
+suits = %w[♦ ♠ ♥ ♣]
 
 puts "#{values.sample} of #{suits.sample}"
